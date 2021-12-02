50182e88afc34269b271f9a730922ac9f0b383d2
Tyler Swanson <31103658+tylerswanson2@users.noreply.github.com>
2021-12-02 18:13:43 -0500

    Add dynamic layouts

--- app/src/main/java/dev/ex4/android/musicwidget/MusicWidgetProvider.kt
+++ app/src/main/java/dev/ex4/android/musicwidget/MusicWidgetProvider.kt
@@ -7,6 +7,7 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.util.SizeF
 import android.widget.RemoteViews
 
 class MusicWidgetProvider : AppWidgetProvider() {
@@ -18,15 +19,51 @@ class MusicWidgetProvider : AppWidgetProvider() {
         if (appWidgetIds == null) return // No widgets
         if (appWidgetManager == null) return
         for (appWidgetId in appWidgetIds) {
-            val views = RemoteViews(context?.packageName, R.layout.music_widget)
 
-            val noti = MusicNotiListener.latestNotification;
+            // Update layout
+            val tinyView = RemoteViews(context?.packageName, R.layout.music_widget_size_1)
+            updateData(context, tinyView)
+            val smallView = RemoteViews(context?.packageName, R.layout.music_widget_size_2)
+            updateData(context, smallView)
+            val mediumView = RemoteViews(context?.packageName, R.layout.music_widget_size_3)
+            updateData(context, mediumView)
+            val largeView = RemoteViews(context?.packageName, R.layout.music_widget)
+            updateData(context, largeView)
+
+            val viewMapping: Map<SizeF, RemoteViews> = mapOf(
+                SizeF(50f, 100f) to tinyView,
+                SizeF(130f, 100f) to smallView,
+                SizeF(200f, 100f) to mediumView,
+                SizeF(270f, 100f) to largeView
+            )
+            val views = RemoteViews(viewMapping)
+            Log.i("MW", "View: ${views.layoutId}")
+
+            // Update information
+            // updateData(context, views)
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun updateData(context: Context?, views: RemoteViews) {
+            val noti = MusicNotiListener.latestNotification
 
             val openSettingsIntent = Intent(context, MainActivity::class.java)
             val openSettingsPendingIntent =
-                PendingIntent.getActivity(context, 0, openSettingsIntent, PendingIntent.FLAG_IMMUTABLE)
+                PendingIntent.getActivity(
+                    context,
+                    0,
+                    openSettingsIntent,
+                    PendingIntent.FLAG_IMMUTABLE
+                )
 
-            views.setOnClickPendingIntent(R.id.settings_button, openSettingsPendingIntent)
+            if (views.layoutId == R.layout.music_widget) views.setOnClickPendingIntent(
+                R.id.settings_button,
+                openSettingsPendingIntent
+            )
 
             if (noti != null) {
 
@@ -35,12 +72,15 @@ class MusicWidgetProvider : AppWidgetProvider() {
                 val subtext = noti.extras.get(Notification.EXTRA_TEXT)
                 val icon = noti.getLargeIcon()
 
-                views.setTextViewText(
-                    R.id.titleText,
-                    // noti.tickerText
-                    "$title\n$subtext"
-                )
-                views.setImageViewIcon(R.id.musicThumbnail, icon)
+                if (sizeMin(views, 3)) {
+                    views.setTextViewText(
+                        R.id.titleText,
+                        // noti.tickerText
+                        "$title"
+                    )
+                    views.setTextViewText(R.id.subtitleText, "$subtext")
+                }
+                if (sizeMin(views, 2)) views.setImageViewIcon(R.id.musicThumbnail, icon)
                 for (key in noti.extras.keySet()) {
                     Log.i("MusicWidgetProvider", key + "=" + noti.extras.get(key).toString())
                 }
@@ -72,34 +112,58 @@ class MusicWidgetProvider : AppWidgetProvider() {
 
                 openAppIntent = noti.contentIntent
 
-                if (backIntent != null) views.setOnClickPendingIntent(R.id.back_button, backIntent)
+                if (sizeMin(
+                        views,
+                        2
+                    ) && backIntent != null
+                ) views.setOnClickPendingIntent(R.id.back_button, backIntent)
                 if (playPauseIntent != null) views.setOnClickPendingIntent(
                     R.id.play_button,
                     playPauseIntent
                 )
-                if (forwardIntent != null) views.setOnClickPendingIntent(
+                if (sizeMin(views, 2) && forwardIntent != null) views.setOnClickPendingIntent(
                     R.id.next_button,
                     forwardIntent
                 )
                 if (openAppIntent != null) {
-                    views.setOnClickPendingIntent(R.id.titleText, openAppIntent)
+                    if (sizeMin(views, 3)) {
+                        views.setOnClickPendingIntent(R.id.titleText, openAppIntent)
+                        views.setOnClickPendingIntent(R.id.subtitleText, openAppIntent)
+                    }
+                    if (sizeMin(views, 2)) views.setOnClickPendingIntent(R.id.musicThumbnail, openAppIntent)
                     views.setOnClickPendingIntent(R.id.music_widget_layout, openAppIntent)
                 }
-                if (playPauseIcon == "pause") views.setImageViewResource(R.id.play_button, R.drawable.pause_icon)
+                if (playPauseIcon == "pause") views.setImageViewResource(
+                    R.id.play_button,
+                    R.drawable.pause_icon
+                )
                 else views.setImageViewResource(R.id.play_button, R.drawable.play_icon)
             } else {
                 // When no music, the text just opens the Music Widget app
-
-                views.setOnClickPendingIntent(R.id.titleText, openSettingsPendingIntent)
                 // If no notification
-                views.setImageViewIcon(R.id.musicThumbnail, null) // Remove icon
-                views.setTextViewText( // Remove text
-                    R.id.titleText,
-                    "No media playing"
-                )
-            }
 
-            appWidgetManager.updateAppWidget(appWidgetId, views)
+                if (sizeMin(views, 3)) {
+                    views.setTextViewText(R.id.titleText, "No media playing")
+                    views.setOnClickPendingIntent(R.id.titleText, openSettingsPendingIntent)
+                    views.setOnClickPendingIntent(R.id.subtitleText, openSettingsPendingIntent)
+                }
+                if (sizeMin(views, 2)) {
+                    views.setOnClickPendingIntent(R.id.musicThumbnail, null)
+                    views.setOnClickPendingIntent(R.id.music_widget_layout, openSettingsPendingIntent)
+                    views.setImageViewIcon(R.id.musicThumbnail, null)
+                }
+            }
+        }
+
+        @JvmStatic
+        fun sizeMin(view: RemoteViews, min: Int): Boolean {
+            if (min == 4) return (view.layoutId == R.layout.music_widget)
+            if (min == 3) return (view.layoutId == R.layout.music_widget || view.layoutId == R.layout.music_widget_size_3)
+            if (min == 2) return (view.layoutId == R.layout.music_widget || view.layoutId == R.layout.music_widget_size_3 || view.layoutId == R.layout.music_widget_size_2)
+            if (min == 1) return (view.layoutId == R.layout.music_widget || view.layoutId == R.layout.music_widget_size_3 || view.layoutId == R.layout.music_widget_size_2 || view.layoutId == R.layout.music_widget_size_1)
+            return false
         }
     }
+
+
 }
